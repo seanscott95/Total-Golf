b9a8501caf9a83ec33140b75a61d4dc79d96a106
Sean <seanms418@gmail.com>
2022-07-19 16:20:12 +1000

    created a navbar folder to hold the navbar jsx component and also the navbar css file, created a navbar that will have its own created burger using three divs to be animated later, created an update menu function that changes the state variables, that deal with the burger and menu classes, to toggle depending on the value of the isMenuCLicked state variable

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+
+    // Changes classes for burger
+    const [burger_class, setBurgerClass] = useState('burger-bar unclicked')
+    const [menu_class, setMenuClass] = useState('menu hidden')
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    // Toggles burger menu
+    const updateMenu = () => {
+      if(!isMenuClicked) {
+        setBurgerClass('burger-bar clicked')
+        setMenuClass('menu visible')
+      } else {
+        setBurgerClass('burger-bar unclicked')
+        setMenuClass('menu hidden')
+      }
+    }
+
+  return (
+    <div className='navbarContainer'>
+        <nav className="navbar">
+            <div className='burger-menu' onClick={updateMenu}>
+                <div className={burger_class}></div>
+                <div className={burger_class}></div>
+                <div className={burger_class}></div>
+            </div>
+        </nav>
+
+        <div className={menu_class}></div>
+    </div>
+  )
+}
+
+export default Navbar
