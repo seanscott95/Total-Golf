c3b2caf1d3dbf45228087403d16eed32210242e6
Sean <seanms418@gmail.com>
2022-08-06 00:02:00 +1000

    created the config file that sets up the mongo db and used it in the server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3001;
+
+connectDB();
 
 const app = express();
 
