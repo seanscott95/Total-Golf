2046c6e49d44b1526afc28d8a63f1bcf1330669e
Sean <seanms418@gmail.com>
2022-08-05 23:18:27 +1000

    installed express async handler and used on scorecard controller

--- server/controllers/scorecardController.js
+++ server/controllers/scorecardController.js
@@ -1,28 +1,33 @@
+const asyncHandler = require('express-async-handler');
+
 // @decription Get scorecard
 // @route GET /api/scores
 // @acess Private
-const getScorecard = (req, res) => {
-    res.status(200).json({ massage: 'Get scores'})
-}
+const getScorecard = asyncHandler(async (req, res) => {
+    res.status(200).json({ massage: 'Get scores'});
+});
 
 // @decription Set scorecard
 // @route POST /api/scores
 // @acess Private
-const setScorecard = (req, res) => {
-    res.status(200).json({ message: 'Create Scorecard'})
-}
+const setScorecard = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Create Scorecard'});
+});
+
 // @decription Update scorecard
 // @route PUT /api/scores/:id
 // @acess Private
-const updateScorecard = (req, res) => {
-    res.status(200).json({ message: `Update Scorecard ${req.params.id}`})
-}
+const updateScorecard = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update Scorecard ${req.params.id}`});
+});
+
 // @decription Delete scorecard
 // @route DELETE /api/scores
 // @acess Private
-const deleteScorecard = (req, res) => {
-    res.status(200).json({ message: `Delete Scorecard ${req.params.id}`})
-}
+const deleteScorecard = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete Scorecard ${req.params.id}`});
+});
+
 module.exports = {
     getScorecard,
     setScorecard,
