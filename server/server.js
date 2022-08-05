d77725a25a33c9416d569eca36a07a070619ead4
Sean <seanms418@gmail.com>
2022-08-05 23:11:35 +1000

    created error middleware, now being used in server file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,5 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/scores', require('./routes/scorecardRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () =>  console.log(`API server running on port ${PORT}!`));
