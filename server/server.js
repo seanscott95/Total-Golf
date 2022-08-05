e78278ac7ed8bfbeca52c8a421866994c4cab6b6
Sean <seanms418@gmail.com>
2022-08-05 23:03:20 +1000

    added express middleware json and urlencoded

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/scores', require('./routes/scorecardRoutes'));
 
 app.listen(PORT, () =>  console.log(`API server running on port ${PORT}!`));
