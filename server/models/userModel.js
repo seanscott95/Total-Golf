09ffd4f44e7c795968ff6859cbb846f158fb1209
Sean <seanms418@gmail.com>
2022-08-08 13:09:16 +1000

    created the user model

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: [true, 'Username already exists'],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: [true, 'Email already exists'],
+        match: [/.+@.+\..+/, 'Please enter a valid email address'],
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: 8,
+        trim: true,
+    },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
