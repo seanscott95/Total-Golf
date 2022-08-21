6f3d09f8a81e15659e099b4311b2e2f5571c6e98
Sean <seanms418@gmail.com>
2022-08-21 18:05:33 +1000

    created form for signup page, onChange and onSubmit functions are empty

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
 
-export default Signup
+  const { name, email, password, password2 } = formData;
+
+  const onChange = () => {
+
+  }
+
+  const onSubmit = () => {
+
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Sign Up</h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name='name'
+              value={name}
+              placeholder='Enter your name'
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="email"
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="password"
+              name='password'
+              value={password}
+              placeholder='Enter your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name='password2'
+              value={password2}
+              placeholder='Confirm password'
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-">
+            <button type='submit' className='btn'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Signup;
