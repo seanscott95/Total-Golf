4aa6a0a0e07c057d9b23fece532a5781ab1b9979
Sean <seanms418@gmail.com>
2022-08-21 17:23:00 +1000

    created empty functional components for each page and added them to the app file as routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Personal from './pages/Personal';
+import Leaderboard from './pages/Leaderboard';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" />
-        <Route path="/Profile" />
-        <Route path="/History" />
-        <Route path="/Leaderboard" />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/Leaderboard" element={<Leaderboard />} />
       </Routes>
     </Router>
   );
