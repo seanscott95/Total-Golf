9bc9e83c99c1c5bd4aca45b9df463f45adb505de
Sean <seanms418@gmail.com>
2022-08-23 15:47:18 +1000

    app file imported the toast container and placed in app's jsx, login file imported useSelector, useDispatch, useNaviagate and react toastify

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
 
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from './pages/Homepage';
@@ -9,16 +10,19 @@ import Leaderboard from './pages/Leaderboard';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/personal" element={<Personal />} />
-        <Route path="/Leaderboard" element={<Leaderboard />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/Leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
