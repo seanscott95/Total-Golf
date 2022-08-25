97b662fbf9e0a1e57398ef6dbd276f7413cc7e81
Sean <seanms418@gmail.com>
2022-08-25 21:03:42 +1000

    changed the username model to a string since option of users as an input has not been developed yet

--- server/models/scoreModel.js
+++ server/models/scoreModel.js
@@ -2,10 +2,15 @@ const { Schema, model } = require('mongoose');
 
 const scoreSchema = new Schema({
     username: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        // required: [true, 'Please add the players username'],
+        type: String,
+        required: [true, 'Please add the players username'],
     },
+    // Username relation has not been set up, for future development
+    // username: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'User',
+    //     required: [true, 'Please add the players username'],
+    // },
     firstNine: {
         type: Array,
         required: [true, 'Please add the score'],
