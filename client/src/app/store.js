417c0c85fa730d63d9825d69dd482e2c40f30f89
Sean <seanms418@gmail.com>
2022-08-25 12:27:08 +1000

    added the scorecard reducer to the redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../utils/auth/authSlice';
+import scorecardReducer from '../utils/scorecard/scorecardSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        scorecard: scorecardReducer,
     },
 });
