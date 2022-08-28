970b1791de5ca17f946de3cc03527e79bf981948
Sean <seanms418@gmail.com>
2022-08-29 00:52:34 +1000

    changed the scorecard model from using the score model as a sub document, score model is now in score card model as a nested object, score model was also changed instead of having an array for the firstNine and lastNine there is now a object with hole 1-9 and holes 10-18

--- server/models/scoreModel.js
+++ server/models/scoreModel.js
@@ -12,12 +12,62 @@ const scoreSchema = new Schema({
     //     required: [true, 'Please add the players username'],
     // },
     firstNine: {
-        type: Array,
-        required: [true, 'Please add the score'],
+        hole1: {
+            type: String,
+        },
+        hole2: {
+            type: String,
+        },
+        hole3: {
+            type: String,
+        },
+        hole4: {
+            type: String,
+        },
+        hole5: {
+            type: String,
+        },
+        hole6: {
+            type: String,
+        },
+        hole7: {
+            type: String,
+        },
+        hole8: {
+            type: String,
+        },
+        hole9: {
+            type: String,
+        },
     },
     lastNine: {
-        type: Array,
-        required: [true, 'Please add the score'],
+        hole10: {
+            type: String,
+        },
+        hole11: {
+            type: String,
+        },
+        hole12: {
+            type: String,
+        },
+        hole13: {
+            type: String,
+        },
+        hole14: {
+            type: String,
+        },
+        hole15: {
+            type: String,
+        },
+        hole16: {
+            type: String,
+        },
+        hole17: {
+            type: String,
+        },
+        hole18: {
+            type: String,
+        },
     },
 },
     {
