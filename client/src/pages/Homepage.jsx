d318675400d031b347e2cbf92e66a046d03d9b58
Sean <seanms418@gmail.com>
2022-08-28 16:18:40 +1000

    using use selector to choose the state scorecard, if that state scorecard is Loading the spinner will appear, id there is an error the message will be displayed, using use dispatch all the scorecards are loaded to the state

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,19 +1,37 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import ScorecardForm from '../components/ScorecardForm/ScorecardForm';
+import { getAllScorecards, reset } from '../utils/scorecard/scorecardSlice';
+import spinner from '../assets/gif/Ghost.gif';
 
 function Homepage() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   
-  const { user } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
+  const { scorecard, isLoading, isError, message } = useSelector((state) => state.scorecard);
 
   useEffect(() => {
     if(!user) {
       navigate('/login');
     };
-  }, [user, navigate]);
+
+    if(isError) {
+      console.log(message);
+    };
+
+    dispatch(getAllScorecards());
+
+    return () => {
+      dispatch(reset());
+    }
+  }, [user, navigate, isError, message, dispatch]);
+
+  if(isLoading) {
+    return <img src={spinner} alt='Loading' />
+  };
 
   return (
     <>
