895ce7ad254d93c3b3a6b519815fabe703ef72b9
Sean <seanms418@gmail.com>
2022-08-29 21:59:03 +1000

    changed the scorecard model score to reference the score model instead of embedding

--- server/models/scorecardModel.js
+++ server/models/scorecardModel.js
@@ -8,68 +8,8 @@ const scorecardSchema = new Schema({
     },
     score: [
         {
-            username: {
-                type: String,
-                required: [true, 'Please add the players username'],
-            },
-            firstNine: {
-                hole1: {
-                    type: String,
-                },
-                hole2: {
-                    type: String,
-                },
-                hole3: {
-                    type: String,
-                },
-                hole4: {
-                    type: String,
-                },
-                hole5: {
-                    type: String,
-                },
-                hole6: {
-                    type: String,
-                },
-                hole7: {
-                    type: String,
-                },
-                hole8: {
-                    type: String,
-                },
-                hole9: {
-                    type: String,
-                },
-            },
-            lastNine: {
-                hole10: {
-                    type: String,
-                },
-                hole11: {
-                    type: String,
-                },
-                hole12: {
-                    type: String,
-                },
-                hole13: {
-                    type: String,
-                },
-                hole14: {
-                    type: String,
-                },
-                hole15: {
-                    type: String,
-                },
-                hole16: {
-                    type: String,
-                },
-                hole17: {
-                    type: String,
-                },
-                hole18: {
-                    type: String,
-                },
-            },
+            type: Schema.Types.ObjectId,
+            ref: 'Score',
         },
     ],
     datePlayed: {
