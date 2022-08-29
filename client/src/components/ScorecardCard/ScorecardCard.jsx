d2af6af061f8b6d0e57e720cd84e4e803618d476
Sean <seanms418@gmail.com>
2022-08-29 22:28:15 +1000

    if scorecard score exists using optional chaining then all the scores in the score array are mapped to a table row that has the username and scores for firstnine and lasnine holes in table data cells that pair with the table head

--- client/src/components/ScorecardCard/ScorecardCard.jsx
+++ client/src/components/ScorecardCard/ScorecardCard.jsx
@@ -1,9 +1,64 @@
 function ScorecardCard({ scorecard }) {
-    console.log(scorecard)
+    // console.log('scorecard---', scorecard)
     return (
         <div className="scorecard">
             <div>
                 {new Date(scorecard.datePlayed).toLocaleString('en-AU')}
+                {scorecard.courseName}
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Hole</th>
+                            <th>1</th>
+                            <th>2</th>
+                            <th>3</th>
+                            <th>4</th>
+                            <th>5</th>
+                            <th>6</th>
+                            <th>7</th>
+                            <th>8</th>
+                            <th>9</th>
+                            <th>10</th>
+                            <th>11</th>
+                            <th>12</th>
+                            <th>13</th>
+                            <th>14</th>
+                            <th>15</th>
+                            <th>16</th>
+                            <th>17</th>
+                            <th>18</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {scorecard.score?.map((item) => (
+                                <tr>
+                                    <td>{item.username}</td>
+                                    <td></td>
+                                    <td>{item.firstNine.hole1}</td>
+                                    <td>{item.firstNine.hole2}</td>
+                                    <td>{item.firstNine.hole3}</td>
+                                    <td>{item.firstNine.hole4}</td>
+                                    <td>{item.firstNine.hole5}</td>
+                                    <td>{item.firstNine.hole6}</td>
+                                    <td>{item.firstNine.hole7}</td>
+                                    <td>{item.firstNine.hole8}</td>
+                                    <td>{item.firstNine.hole9}</td>
+                                    <td>{item.lastNine.hole10}</td>
+                                    <td>{item.lastNine.hole11}</td>
+                                    <td>{item.lastNine.hole12}</td>
+                                    <td>{item.lastNine.hole13}</td>
+                                    <td>{item.lastNine.hole14}</td>
+                                    <td>{item.lastNine.hole15}</td>
+                                    <td>{item.lastNine.hole16}</td>
+                                    <td>{item.lastNine.hole17}</td>
+                                    <td>{item.lastNine.hole18}</td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
