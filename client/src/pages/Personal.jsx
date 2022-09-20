2cf0f2474e0eddc96583b0b572d3b946f57b43f2
Sean <seanms418@gmail.com>
2022-09-20 20:50:24 +1000

    personal page displays the ScoreCard component that displays a single score. On the personal page the score that is being displayed is the personal redux state which displays the current users scores

--- client/src/pages/Personal.jsx
+++ client/src/pages/Personal.jsx
@@ -1,20 +1,59 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
+import ScoreCard from '../components/ScoreCard/ScoreCard';
+import { getAllPersonal, reset } from '../utils/personal/personalSlice';
+import spinner from '../assets/gif/Ghost.gif';
 
 function Personal() {
   const navigate = useNavigate();
-  
-  const { user } = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { personal, isLoading, isError, message } = useSelector((state) => state.personal);
 
   useEffect(() => {
-    if(!user) {
+    if (!user) {
       navigate('/login');
     };
-  }, [user, navigate]);
+
+    if (isError) {
+      console.log(`Error: ${message}`);
+    };
+
+    if (user) {
+      dispatch(getAllPersonal(user.username));
+    };
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
 
   return (
-    <div>Personal</div>
+    <div>
+      <section className="heading">
+        <h1>Personal Scores</h1>
+        <p>Welcome {user && user.username} !</p>
+      </section>
+
+      {isLoading ? (
+        <img src={spinner} alt='Loading' />
+      ) : (
+        <section className='content'>
+          {personal.length > 0 ? (
+            <div className='scores'>
+              {personal.map((item) => (
+                <ScoreCard key={item._id} score={item} />
+              )).reverse()}
+            </div>
+          ) : (
+            <h3>There are no scorecards!</h3>
+          )}
+        </section>
+      )}
+    </div>
   )
 }
 
