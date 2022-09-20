8762b932ed51e4d420c1c6e646c9ab07af7fd89b
Sean <seanms418@gmail.com>
2022-09-20 18:35:10 +1000

    created redux state named personal

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../utils/auth/authSlice';
 import scorecardReducer from '../utils/scorecard/scorecardSlice';
+import personalReducer from '../utils/personal/personalSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         scores: scorecardReducer,
+        personal: personalReducer,
     },
 });
