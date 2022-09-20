b0a7430b470697c28f00c5a65bb6643d70eb8454
Sean <seanms418@gmail.com>
2022-09-20 18:33:58 +1000

    addded the personal route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const scorecardRoutes = require('./scorecardRoutes');
 const scoreRoutes = require('./scoreRoutes');
+const personalRoutes = require('./personalRoutes');
 
 router.use('/api/users', userRoutes);
 router.use('/api/scores', scorecardRoutes);
 router.use('/api/score', scoreRoutes);
+router.use('/api/personal', personalRoutes);
 
 module.exports = router;
