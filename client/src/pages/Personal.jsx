d0ba0cb65917345898514314ee227f11ab4b1eaa
Sean <seanms418@gmail.com>
2022-09-27 16:21:49 +1000

    created a filteredScore variable that filters through the scores redux state and returns all the score objects that the user is in, jsx created uses the scorecardCard component to display all of the scorecards the user is in

--- client/src/pages/Personal.jsx
+++ client/src/pages/Personal.jsx
@@ -3,7 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import ScoreCard from '../components/ScoreCard/ScoreCard';
-import { getAllPersonal, reset } from '../utils/personal/personalSlice';
+import ScorecardCard from '../components/ScorecardCard/ScorecardCard';
+import { getAllPersonal, resetPersonal } from '../utils/personal/personalSlice';
+import { getAllScorecards, reset } from '../utils/scorecard/scorecardSlice';
 import spinner from '../assets/gif/Ghost.gif';
 
 function Personal() {
@@ -12,6 +14,12 @@ function Personal() {
 
   const { user } = useSelector((state) => state.auth);
   const { personal, isLoading, isError, message } = useSelector((state) => state.personal);
+  const { scores, scoresIsLoading, scoresIsError, scoresMessage } = useSelector((state) => state.scores);
+
+  // Filters the scores array and returns all objects if the user is on the scorecard
+  const filteredScores = scores.filter(obj => {
+    return obj.score.some(item => item.username === user.username);
+  });
 
   useEffect(() => {
     if (!user) {
@@ -27,12 +35,30 @@ function Personal() {
     };
 
     return () => {
-      dispatch(reset());
+      dispatch(resetPersonal());
     };
   }, [user, navigate, isError, message, dispatch]);
 
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    };
+
+    if (scoresIsError) {
+      console.log(`Error: ${scoresMessage}`);
+    };
+
+    if (user) {
+      dispatch(getAllScorecards());
+    };
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, scoresIsError, scoresMessage, dispatch]);
+
   return (
-    <div>
+    <>
       <section className="heading">
         <h1>Personal Scores</h1>
         <p>Welcome {user && user.username} !</p>
@@ -41,23 +67,56 @@ function Personal() {
       {isLoading ? (
         <img src={spinner} alt='Loading' />
       ) : (
-        <section className='content'>
-          <div className='section-heading'>
-            <h3>ALL</h3>
-            <p>Below are all of your previous scores</p>
-          </div>
-          {personal.length > 0 ? (
-            <div className='scores'>
-              {personal.map((item) => (
-                <ScoreCard key={item._id} score={item} />
-              )).reverse()}
+        <>
+          <section className='content'>
+            <div className='section-heading'>
+              <h3>ALL</h3>
+              <p>Below are all of your previous scores</p>
             </div>
-          ) : (
-            <h3>You have no scores!</h3>
-          )}
-        </section>
+
+            {personal.length > 0 ? (
+              <div className='scores'>
+                {personal.map((item) => (
+                  <ScoreCard key={item._id} score={item} />
+                )).reverse()}
+              </div>
+            ) : (
+              <h3>You have no scores!</h3>
+            )}
+          </section>
+        </>
       )}
-    </div>
+
+      {scoresIsLoading ? (
+        <img src={spinner} alt='Loading' />
+      ) : (
+        <>
+          <section className='content'>
+            <div className='section-heading'>
+              <h3>ALL GAMES</h3>
+              <p>Here are all the games you've be in</p>
+            </div>
+
+            {filteredScores.length > 0 ? (
+              <div className='scores'>
+                {filteredScores.map((item) => (
+                  <ScorecardCard key={item._id} scorecard={item} />
+                )).reverse()}
+              </div>
+            ) : (
+              <h3>You have no scores!</h3>
+            )}
+          </section>
+
+          <section className='content'>
+            <div className='section-heading'>
+              <h3>BEST AND BLURST</h3>
+              <p>Lets see your best and worst scores</p>
+            </div>
+          </section>
+        </>
+      )}
+    </>
   )
 }
 
