4e9e310c2e716f4f8e7160a28ba879ee841a5e03
Sean <seanms418@gmail.com>
2023-02-02 00:51:47 +1100

    styled the component

--- client/src/components/NewQPCourseButton/NewQPCourseButton.jsx
+++ client/src/components/NewQPCourseButton/NewQPCourseButton.jsx
@@ -5,15 +5,15 @@ import golfCroc from '../../assets/svg/Crocodile-Playing-Golf.svg';
 
 function NewQPCourseButton() {
   return (
-    <div className="container">
+    <div className="container-button">
         <section className="">
             <h1>Queens Park</h1>
             <p>Create a new Scorecard for Queens Park</p>
             <Link to="/CreateScorecardQP">
-                <button >GET STARTED &gt;</button>
+                <button className="btn">GET STARTED &gt;</button>
             </Link>
         </section>
-        <img src={golfCroc} alt="Cartoon Golf Crocodile" />
+        <img src={golfCroc} alt="Cartoon Golf Crocodile" className="croc-svg" />
     </div>
   )
 }
