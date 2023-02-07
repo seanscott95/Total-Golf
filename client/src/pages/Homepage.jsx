5dd4057ad21a55add4f2bcf458d4f5176bb4a542
Sean <seanms418@gmail.com>
2023-02-07 15:49:47 +1100

    implemented the NewCourseButton component which also required making an abject of the unique information to pass down as props to child

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,44 @@
 import PhotoMontage from '../components/PhotoMontage/PhotoMontage';
-import NewQPCourseButton from '../components/NewQPCourseButton/NewQPCourseButton';
+import NewCourseButton from '../components/NewCourseButton/NewCourseButton';
+
+import GolfCroc from '../assets/svg/GolfCroc.svg';
+import GolfEagle from '../assets/svg/GolfEagle.png';
+
 function Homepage() {
-  
+  // Values for the New Course Button
+  const newCourse = {
+    header: 'Any Golf Course',
+    text: 'any golf course',
+    link: '/CreateScorecard',
+    image: GolfCroc,
+  };
+
+  // Values for the Queens Park Course Button
+  const queensPark = {
+    header: 'Queens Park',
+    text: 'Queens Park',
+    link: '/CreateScorecardQP',
+    image: GolfEagle,
+  };
+
   return (
     <>
       <PhotoMontage />
-      <NewQPCourseButton />
+      
+      <section className='course-buttons'>
+        <NewCourseButton 
+          header={queensPark.header}
+          text={queensPark.text}
+          link={queensPark.link}
+          image={queensPark.image}
+        />
+        <NewCourseButton 
+          header={newCourse.header}
+          text={newCourse.text}
+          link={newCourse.link}
+          image={newCourse.image}
+        />
+      </section>
     </>
   )
 }
