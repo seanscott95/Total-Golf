02b208c38c970b74f5f07cbf59c8f5032b1c5c4f
Sean <seanms418@gmail.com>
2023-02-22 14:06:00 +1100

    fixed builder reducer functions logout that had a syntax error with the parameter brackets, this has been affecting the logout state for the user

--- client/src/utils/auth/authSlice.js
+++ client/src/utils/auth/authSlice.js
@@ -88,9 +88,9 @@ export const authSlice = createSlice({
                 state.message = action.payload
                 state.user = null
             })
-            .addCase(logout.fulfilled, (state => {
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null
-            }))
+            })
     },
 });
 
