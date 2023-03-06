d4bc93d5347df36e894a6fb437a1c4d26a4cc939
Sean <seanms418@gmail.com>
2023-03-06 16:44:37 +1100

    totalscore function edited to be able to also send back the total of both firstNine and lastNine holes or just either firstNine or lastNine holes

--- client/src/utils/helper/totalScore.js
+++ client/src/utils/helper/totalScore.js
@@ -1,11 +1,24 @@
 // Calculates the total score of the firstNine and lastNine holes values
 export const totalScore = (firstNine, lastNine) => {
     let sum = 0;
-    for (const value in firstNine) {
-        sum += parseInt(firstNine[value]);
+    // Tally firstNine and lastNine if they are both truthy
+    if (firstNine.hole1 > 0 && lastNine.hole10 > 0) {
+        for (const value in firstNine) {
+            sum += parseInt(firstNine[value]);
+        };
+        for (const value2 in lastNine) {
+            sum += parseInt(lastNine[value2]);
+        };
+        return sum;
+    } else if (firstNine.hole1 > 0) {
+        for (const value in firstNine) {
+            sum += parseInt(firstNine[value]);
+        };
+        return sum;
+    } else if (lastNine.hole10 > 0) {
+        for (const value2 in lastNine) {
+            sum += parseInt(lastNine[value2]);
+        };
+        return sum;
     };
-    for (const value2 in lastNine) {
-        sum += parseInt(lastNine[value2]);
-    };
-    return sum
 };
