02d59ba7a7858cf483e017479cd41329a7b72e82
Sean <seanms418@gmail.com>
2023-04-04 16:21:55 +1000

    created a site password to be able to sign up by using environmental variables for the site password to be checked against the site password input, if correct the sign up form renders, also replaced the anchor tag to sign in as a link element from react router dom allowing the link to not send away from the app

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { signup, reset } from '../utils/auth/authSlice';
 import spinner from '../assets/gif/Ghost.gif';
 
 function Signup() {
+  const [sitePassword, setSitePassword] = useState("");
+  const [isVerified, setIsVerified] = useState(false);
+
   const [formData, setFormData] = useState({
     username: '',
     email: '',
@@ -30,29 +33,41 @@ function Signup() {
     };
 
     dispatch(reset());
-  }, [user, isError, isSuccess, message, navigate, dispatch])
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  
+  const sitePasswordOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (process.env.REACT_APP_SITE_PASSWORD === sitePassword) {
+      setIsVerified((current) => !current);
+    } else {
+      setSitePassword("");
+      toast.error('Password is incorrect');
+    };
+  };
 
   const onChange = (e) => {
     setFormData((prev) => ({
       ...prev,
       [e.target.name]: e.target.value
-    }))
-  }
+    }));
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     if (password !== password2) {
-      toast.error('Passwords do not match')
+      toast.error('Passwords do not match');
     } else {
       const userData = {
         username,
         email,
         password
       };
-      dispatch(signup(userData))
-    }
-  }
+      dispatch(signup(userData));
+    };
+  };
 
   if (isLoading) {
     return <img src={spinner} alt='Loading' className="spinner" />
@@ -61,71 +76,101 @@ function Signup() {
   return (
     <div className="signup-page">
       <div className='signin-container'>
-        <section className="heading">
-          <h1>Sign Up</h1>
-          <p>Please create an account</p>
-        </section>
+        {!isVerified ? (
+          <>
+            <section className="heading">
+              <h1>Sign Up</h1>
+              <p>Please enter the site password</p>
+            </section>
 
-        <section className="form">
-          <form onSubmit={onSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                name='username'
-                value={username}
-                placeholder='Name'
-                onChange={onChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                name='email'
-                value={email}
-                placeholder='Email'
-                onChange={onChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                name='password'
-                value={password}
-                placeholder='Password'
-                onChange={onChange}
-                minLength='8'
-                required
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                id="password2"
-                name='password2'
-                value={password2}
-                placeholder='Confirm password'
-                onChange={onChange}
-                minLength='8'
-                required
-              />
-            </div>
-            <div>
-              <p>Already have an account? <a href="/signin">Sign in here.</a></p>
-            </div>
-            <div className="form-group">
-              <button type='submit' className='btn-square'>Submit</button>
-            </div>
-          </form>
-        </section>
+            <section >
+              <form onSubmit={sitePasswordOnSubmit}>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    id="site-password"
+                    name="site-password"
+                    placeholder="Site Password"
+                    value={sitePassword}
+                    onChange={(e) => setSitePassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <button type='submit' className='btn-square'>Submit</button>
+                </div>
+              </form>
+            </section>
+          </>
+        ) : (
+          <>
+            <section className="heading">
+              <h1>Sign Up</h1>
+              <p>Please create an account</p>
+            </section>
+
+            <section className="form">
+              <form onSubmit={onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    name='username'
+                    value={username}
+                    placeholder='Name'
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name='email'
+                    value={email}
+                    placeholder='Email'
+                    onChange={onChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name='password'
+                    value={password}
+                    placeholder='Password'
+                    onChange={onChange}
+                    minLength='8'
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password2"
+                    name='password2'
+                    value={password2}
+                    placeholder='Confirm password'
+                    onChange={onChange}
+                    minLength='8'
+                    required
+                  />
+                </div>
+                <div>
+                  <p>Already have an account? <Link to='/signin'>Sign in here.</Link></p>
+                </div>
+                <div className="form-group">
+                  <button type='submit' className='btn-square'>Submit</button>
+                </div>
+              </form>
+            </section>
+          </>
+        )}
       </div>
     </div>
   );
