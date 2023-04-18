738535ca279fe61f802142354e1f3cd62232b1f5
Sean <seanms418@gmail.com>
2023-04-18 19:04:15 +1000

    edited the scorecardCard and scorecard to display the 1-18 holes with the first nine and last nine scores stacked instead of in a row

--- client/src/components/ScoreCard/ScoreCard.jsx
+++ client/src/components/ScoreCard/ScoreCard.jsx
@@ -32,10 +32,10 @@ const ScoreCard = ({ score }) => {
                 <table className='styled-table'>
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Hole</th>
                             {isFirstNine ?
                                 <>
+                                    <th>Name</th>
+                                    <th className='hideHole'>Hole</th>
                                     <th>1</th>
                                     <th>2</th>
                                     <th>3</th>
@@ -45,10 +45,13 @@ const ScoreCard = ({ score }) => {
                                     <th>7</th>
                                     <th>8</th>
                                     <th>9</th>
+                                    <th>T</th>
                                 </> : <></>
                             }
                             {isLastNine ?
                                 <>
+                                    <th>Name</th>
+                                    <th className='hideHole'>Hole</th>
                                     <th>10</th>
                                     <th>11</th>
                                     <th>12</th>
@@ -58,39 +61,18 @@ const ScoreCard = ({ score }) => {
                                     <th>16</th>
                                     <th>17</th>
                                     <th>18</th>
+                                    <th>T</th>
                                 </> : <></>
                             }
-                            {isBothNine ?
-                                <>
-                                    <th>1</th>
-                                    <th>2</th>
-                                    <th>3</th>
-                                    <th>4</th>
-                                    <th>5</th>
-                                    <th>6</th>
-                                    <th>7</th>
-                                    <th>8</th>
-                                    <th>9</th>
-                                    <th>10</th>
-                                    <th>11</th>
-                                    <th>12</th>
-                                    <th>13</th>
-                                    <th>14</th>
-                                    <th>15</th>
-                                    <th>16</th>
-                                    <th>17</th>
-                                    <th>18</th>
-                                </> : <></>
-                            }
-                            <th>T</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr key={score._id}>
-                            <td>{score.username.charAt(0).toUpperCase() + score.username.slice(1)}</td>
-                            <td>&nbsp;</td>
+
                             {isFirstNine ?
                                 <>
+                                    <td>{score.username.charAt(0).toUpperCase() + score.username.slice(1)}</td>
+                                    <td className='hideHole'>&nbsp;</td>
                                     <td>{score.firstNine.hole1}</td>
                                     <td>{score.firstNine.hole2}</td>
                                     <td>{score.firstNine.hole3}</td>
@@ -100,10 +82,13 @@ const ScoreCard = ({ score }) => {
                                     <td>{score.firstNine.hole7}</td>
                                     <td>{score.firstNine.hole8}</td>
                                     <td>{score.firstNine.hole9}</td>
+                                    <td>{score.total}</td>
                                 </> : <></>
                             }
                             {isLastNine ?
                                 <>
+                                    <td>{score.username.charAt(0).toUpperCase() + score.username.slice(1)}</td>
+                                    <td className='hideHole'>&nbsp;</td>
                                     <td>{score.lastNine.hole10}</td>
                                     <td>{score.lastNine.hole11}</td>
                                     <td>{score.lastNine.hole12}</td>
@@ -113,35 +98,81 @@ const ScoreCard = ({ score }) => {
                                     <td>{score.lastNine.hole16}</td>
                                     <td>{score.lastNine.hole17}</td>
                                     <td>{score.lastNine.hole18}</td>
+                                    <td>{score.total}</td>
                                 </> : <></>
                             }
-                            {isBothNine ?
-                                <>
-                                    <td>{score.firstNine.hole1}</td>
-                                    <td>{score.firstNine.hole2}</td>
-                                    <td>{score.firstNine.hole3}</td>
-                                    <td>{score.firstNine.hole4}</td>
-                                    <td>{score.firstNine.hole5}</td>
-                                    <td>{score.firstNine.hole6}</td>
-                                    <td>{score.firstNine.hole7}</td>
-                                    <td>{score.firstNine.hole8}</td>
-                                    <td>{score.firstNine.hole9}</td>
-                                    <td>{score.lastNine.hole10}</td>
-                                    <td>{score.lastNine.hole11}</td>
-                                    <td>{score.lastNine.hole12}</td>
-                                    <td>{score.lastNine.hole13}</td>
-                                    <td>{score.lastNine.hole14}</td>
-                                    <td>{score.lastNine.hole15}</td>
-                                    <td>{score.lastNine.hole16}</td>
-                                    <td>{score.lastNine.hole17}</td>
-                                    <td>{score.lastNine.hole18}</td>
-                                </> : <></>
-                            }
-                            <td>{score.total}</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            {isBothNine ?
+                <>
+                    <table className='styled-table bigTable'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th className='hideHole'>Hole</th>
+                                <th>1</th>
+                                <th>2</th>
+                                <th>3</th>
+                                <th>4</th>
+                                <th>5</th>
+                                <th>6</th>
+                                <th>7</th>
+                                <th>8</th>
+                                <th>9</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{score.username.charAt(0).toUpperCase() + score.username.slice(1)}</td>
+                                <td className='hideHole'>&nbsp;</td>
+                                <td>{score.firstNine.hole1}</td>
+                                <td>{score.firstNine.hole2}</td>
+                                <td>{score.firstNine.hole3}</td>
+                                <td>{score.firstNine.hole4}</td>
+                                <td>{score.firstNine.hole5}</td>
+                                <td>{score.firstNine.hole6}</td>
+                                <td>{score.firstNine.hole7}</td>
+                                <td>{score.firstNine.hole8}</td>
+                                <td>{score.firstNine.hole9}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <table className='styled-table bigTable'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th className='hideHole'>Hole</th>
+                                <th>10</th>
+                                <th>11</th>
+                                <th>12</th>
+                                <th>13</th>
+                                <th>14</th>
+                                <th>15</th>
+                                <th>16</th>
+                                <th>17</th>
+                                <th>18</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{score.username.charAt(0).toUpperCase() + score.username.slice(1)}</td>
+                                <td className='hideHole'>&nbsp;</td>
+                                <td>{score.lastNine.hole10}</td>
+                                <td>{score.lastNine.hole11}</td>
+                                <td>{score.lastNine.hole12}</td>
+                                <td>{score.lastNine.hole13}</td>
+                                <td>{score.lastNine.hole14}</td>
+                                <td>{score.lastNine.hole15}</td>
+                                <td>{score.lastNine.hole16}</td>
+                                <td>{score.lastNine.hole17}</td>
+                                <td>{score.lastNine.hole18}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </> : <></>}
         </div>
     );
 };
--- client/src/components/ScorecardCard/ScorecardCard.jsx
+++ client/src/components/ScorecardCard/ScorecardCard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { date_all } from '../../utils/helper/dateHelper';
 
 import { FaRegEdit } from 'react-icons/fa';
+import ScoreCard from '../ScoreCard/ScoreCard';
 
 const ScorecardCard = ({ scorecard, showEditBtn, setIsEditMode }) => {
     const navigate = useNavigate();
@@ -20,25 +21,46 @@ const ScorecardCard = ({ scorecard, showEditBtn, setIsEditMode }) => {
         navigate(`/viewScorecard/${id}`);
     };
 
+    const splitBothNineScores = (arr) => {
+        const res = arr.map((item) => ({
+            firstNine: {
+                name: item.username,
+                score: item.firstNine,
+            },
+            lastNine: {
+                name: item.username,
+                score: item.lastNine,
+            }
+        }));
+
+        const fNGrouped = res.map((item) => item.firstNine)
+        const lNGrouped = res.map((item) => item.lastNine)
+        const scores = [{
+            fN: fNGrouped,
+            lN: lNGrouped,
+        }];
+        return scores;
+    };
+
     return (
         <div className='scorecard' onClick={handleScorecardClick}>
             <div className='scorecard-header'>
                 <p><span>Course:</span> {scorecard?.courseName}</p>
                 <p><span>Date:</span> {date_all(scorecardDate)}</p>
-                {showEditBtn ? 
-                <button type='button' className='edit-btn' onClick={setIsEditMode}>
-                    <FaRegEdit />
-                </button>
-                : <></>}
+                {showEditBtn ?
+                    <button type='button' className='edit-btn' onClick={setIsEditMode}>
+                        <FaRegEdit />
+                    </button>
+                    : <></>}
             </div>
             <div>
                 <table className='styled-table'>
                     <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Hole</th>
                             {isFirstNine ?
                                 <>
+                                    <th>Name</th>
+                                    <th className='hideHole'>Hole</th>
                                     <th>1</th>
                                     <th>2</th>
                                     <th>3</th>
@@ -48,10 +70,13 @@ const ScorecardCard = ({ scorecard, showEditBtn, setIsEditMode }) => {
                                     <th>7</th>
                                     <th>8</th>
                                     <th>9</th>
+                                    <th>T</th>
                                 </> : <></>
                             }
                             {isLastNine ?
                                 <>
+                                    <th>Name</th>
+                                    <th className='hideHole'>Hole</th>
                                     <th>10</th>
                                     <th>11</th>
                                     <th>12</th>
@@ -61,40 +86,19 @@ const ScorecardCard = ({ scorecard, showEditBtn, setIsEditMode }) => {
                                     <th>16</th>
                                     <th>17</th>
                                     <th>18</th>
+                                    <th>T</th>
                                 </> : <></>
                             }
-                            {isBothNine ?
-                                <>
-                                    <th>1</th>
-                                    <th>2</th>
-                                    <th>3</th>
-                                    <th>4</th>
-                                    <th>5</th>
-                                    <th>6</th>
-                                    <th>7</th>
-                                    <th>8</th>
-                                    <th>9</th>
-                                    <th>10</th>
-                                    <th>11</th>
-                                    <th>12</th>
-                                    <th>13</th>
-                                    <th>14</th>
-                                    <th>15</th>
-                                    <th>16</th>
-                                    <th>17</th>
-                                    <th>18</th>
-                                </> : <></>
-                            }
-                            <th>T</th>
                         </tr>
                     </thead>
                     <tbody>
                         {scorecard?.score.map((item) => (
                             <tr key={item._id}>
-                                <td>{item.username.charAt(0).toUpperCase() + item.username.slice(1)}</td>
-                                <td>&nbsp;</td>
+
                                 {isFirstNine ?
                                     <>
+                                        <td>{item.username.charAt(0).toUpperCase() + item.username.slice(1)}</td>
+                                        <td className='hideHole'>&nbsp;</td>
                                         <td>{item.firstNine.hole1}</td>
                                         <td>{item.firstNine.hole2}</td>
                                         <td>{item.firstNine.hole3}</td>
@@ -104,10 +108,13 @@ const ScorecardCard = ({ scorecard, showEditBtn, setIsEditMode }) => {
                                         <td>{item.firstNine.hole7}</td>
                                         <td>{item.firstNine.hole8}</td>
                                         <td>{item.firstNine.hole9}</td>
+                                        <td>{item.total}</td>
                                     </> : <></>
                                 }
                                 {isLastNine ?
                                     <>
+                                        <td>{item.username.charAt(0).toUpperCase() + item.username.slice(1)}</td>
+                                        <td className='hideHole'>&nbsp;</td>
                                         <td>{item.lastNine.hole10}</td>
                                         <td>{item.lastNine.hole11}</td>
                                         <td>{item.lastNine.hole12}</td>
@@ -117,36 +124,91 @@ const ScorecardCard = ({ scorecard, showEditBtn, setIsEditMode }) => {
                                         <td>{item.lastNine.hole16}</td>
                                         <td>{item.lastNine.hole17}</td>
                                         <td>{item.lastNine.hole18}</td>
+                                        <td>{item.total}</td>
                                     </> : <></>
                                 }
-                                {isBothNine ?
-                                    <>
-                                        <td>{item.firstNine.hole1}</td>
-                                        <td>{item.firstNine.hole2}</td>
-                                        <td>{item.firstNine.hole3}</td>
-                                        <td>{item.firstNine.hole4}</td>
-                                        <td>{item.firstNine.hole5}</td>
-                                        <td>{item.firstNine.hole6}</td>
-                                        <td>{item.firstNine.hole7}</td>
-                                        <td>{item.firstNine.hole8}</td>
-                                        <td>{item.firstNine.hole9}</td>
-                                        <td>{item.lastNine.hole10}</td>
-                                        <td>{item.lastNine.hole11}</td>
-                                        <td>{item.lastNine.hole12}</td>
-                                        <td>{item.lastNine.hole13}</td>
-                                        <td>{item.lastNine.hole14}</td>
-                                        <td>{item.lastNine.hole15}</td>
-                                        <td>{item.lastNine.hole16}</td>
-                                        <td>{item.lastNine.hole17}</td>
-                                        <td>{item.lastNine.hole18}</td>
-                                    </> : <></>
-                                }
-                                <td>{item.total}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
+            {isBothNine ?
+                <>
+                    {/* {scorecard.score.map((item) => <ScoreCard score={item} />)} */}
+                    {splitBothNineScores(scorecard?.score).map((item, index) => (
+                        <div key={index}>
+                            <table className='styled-table bigTable'>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th className='hideHole'>Hole</th>
+                                        <th>1</th>
+                                        <th>2</th>
+                                        <th>3</th>
+                                        <th>4</th>
+                                        <th>5</th>
+                                        <th>6</th>
+                                        <th>7</th>
+                                        <th>8</th>
+                                        <th>9</th>
+                                    </tr>
+                                </thead>
+                                <tbody >
+                                    {item.fN.map((i) => (
+                                        <tr>
+                                            <td>{i.name.charAt(0).toUpperCase() + i.name.slice(1)}</td>
+                                            <td className='hideHole'>&nbsp;</td>
+                                            <td>{i.score.hole1}</td>
+                                            <td>{i.score.hole2}</td>
+                                            <td>{i.score.hole3}</td>
+                                            <td>{i.score.hole4}</td>
+                                            <td>{i.score.hole5}</td>
+                                            <td>{i.score.hole6}</td>
+                                            <td>{i.score.hole7}</td>
+                                            <td>{i.score.hole8}</td>
+                                            <td>{i.score.hole9}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+                            <table className='styled-table bigTable'>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th className='hideHole'>Hole</th>
+                                        <th>10</th>
+                                        <th>11</th>
+                                        <th>12</th>
+                                        <th>13</th>
+                                        <th>14</th>
+                                        <th>15</th>
+                                        <th>16</th>
+                                        <th>17</th>
+                                        <th>18</th>
+                                    </tr>
+                                </thead>
+                                <tbody >
+                                    {item.lN.map((i) => (
+                                        <tr>
+                                            <td>{i.name.charAt(0).toUpperCase() + i.name.slice(1)}</td>
+                                            <td className='hideHole'>&nbsp;</td>
+                                            <td>{i.score.hole10}</td>
+                                            <td>{i.score.hole11}</td>
+                                            <td>{i.score.hole12}</td>
+                                            <td>{i.score.hole13}</td>
+                                            <td>{i.score.hole14}</td>
+                                            <td>{i.score.hole15}</td>
+                                            <td>{i.score.hole16}</td>
+                                            <td>{i.score.hole17}</td>
+                                            <td>{i.score.hole18}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    ))}
+                </> : <></>}
         </div>
     );
 };
